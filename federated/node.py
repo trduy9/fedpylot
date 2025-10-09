e6331c89aba28c8cf6f4533465b0c483c7de7fc3
Duy Vu <Duy.VPT233369@sis.hust.edu.vn>
2025-10-09 08:24:05 +0700

    Thêm đường dẫn /fedpylot ở test function trong node.py

--- federated/node.py
+++ federated/node.py
@@ -197,7 +197,7 @@ class Node:
         weights = f'{saving_path}/weights/eval-kround{kround}.pt'
         torch.save(self._ckpt_reparam, weights)
         os.system(
-            f'python ./yolov7/test.py'
+            f'python ./fedpylot/yolov7/test.py'
             f' --kround {kround}'
             f' --saving-path {saving_path}'
             f' --weights {weights}'
