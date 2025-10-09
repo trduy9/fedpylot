95a3317f767d213f200ed15038231ebe1fa792b5
Duy Vu <Duy.VPT233369@sis.hust.edu.vn>
2025-10-09 08:50:38 +0700

    Thêm weight_only=False ở torch.load()

--- federated/node.py
+++ federated/node.py
@@ -505,7 +505,7 @@ class Client(Node):
             begin_weights = f'{saving_path}/run/train-client{self.rank}/weights/last.pt'
             torch.save(self._ckpt, begin_weights)
             os.system(f'python {script_path} --resume {begin_weights}')
-        new_ckpt = torch.load(end_weights, map_location=self.device)
+        new_ckpt = torch.load(end_weights, map_location=self.device, weights_only=False)
         # Compute the local update: delta_it = w_t - w_it
         w_it = new_ckpt['model'].state_dict()
         if kround == 0:
